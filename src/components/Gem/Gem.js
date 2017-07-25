ad3cfd02bcc8e70c9acfa9911347b5a31a36d288
Jessica <jdhpabst@gmail.com>
2017-07-25 14:27:21 -0600

    working on sign components

---
+++ src/components/Gem/Gem.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+import './Gem.css';
+
+class Gem extends Component {
+
+  render() {
+    return (
+      <div className="home">
+        <div id="animations">
+          <div className="stars"></div>
+          <div className="twinkling"></div>
+          {/* <div className="clouds"></div>  */}
+        </div>
+        <Link to='/' style={{textDecoration: 'none'}}>
+          <div id="home_title" className="fadeIn">
+            <h1 className="floating">gemini</h1>
+          </div>
+        </Link>
+        <div id="gem_info">
+        </div>
+         <div id="sign_info_container">
+          <h2>Testing the div space...</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default Gem;
