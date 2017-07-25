cee38921ab84b1082452ffde84a996deba6ae3dc
Jessica <jdhpabst@gmail.com>
2017-07-25 14:21:30 -0600

    working on sign components

---
+++ src/components/Leo/Leo.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+import './Leo.css';
+
+class Leo extends Component {
+
+  render() {
+    return (
+      <div className="home">
+        <div id="animations">
+          <div className="stars"></div>
+          <div className="twinkling"></div>
+          {/* <div className="clouds"></div>  */}
+        </div>
+        <Link to='/' style={{textDecoration: 'none'}}>
+          <div id="home_title" className="fadeIn">
+            <h1 className="floating">leo</h1>
+          </div>
+        </Link>
+        <div id="leo_info">
+        </div>
+         <div id="sign_info_container">
+          <h2>Testing the div space...</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default Leo;
