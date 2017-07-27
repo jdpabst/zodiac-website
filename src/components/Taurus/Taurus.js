87bbff673485c57ad9ad5c2f0f72c180fcb4e2b9
Jessica <jdhpabst@gmail.com>
2017-07-27 16:30:35 -0600

    working on styling db info

--- src/components/Taurus/Taurus.js
+++ src/components/Taurus/Taurus.js
@@ -5,7 +5,24 @@ import './Taurus.css';
 
 class Taurus extends Component {
 
+     constructor(props){
+      super(props);
+      this.state = {
+        signsArr: [{}]
+      }
+    }
+
+    componentDidMount(){
+        axios.get(`/api/signs/taurus`).then( signsArr => {
+            this.setState({
+               signsArr: signsArr.data,
+            })
+            console.log(signsArr)
+        })
+    }
+
   render() {
+    let signs = this.state.signsArr;
     return (
       <div className="home">
         <div id="animations">
@@ -21,7 +38,16 @@ class Taurus extends Component {
         <div id="taurus_info">
         </div>
          <div id="sign_info_container">
-          <h2>Testing the div space...</h2>
+          {signs.map( (sign, id) => {
+            return <div key={ id }id="sign_db_info">
+              <img className='planet'src={ signs[id].planet } />
+              <div id="birth_constellation">
+                <h2 className='birth'>{ signs[id].birthday }</h2>
+                <img className='constellation' src={ signs[id].constellation }/>
+              </div>
+              <p className='history'>{ signs[id].history }</p>
+            </div>
+          })}
         </div>
       </div>
     );
